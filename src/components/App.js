7ee424f34106250a5f2fa82dc6f930680252f66f
NamanZelawat <namanzelawat@gmail.com>
2020-10-20 15:03:30 +0530

    done

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React, {Component, useState} from "react";
+import "./../styles/App.css";
+
+function App() 
+{
+  return (
+  <div id="main">
+    //Don not altem main div
+  </div>
+  );
+}
+
+
+export default App;
