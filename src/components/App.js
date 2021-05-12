3f806fc3af0d39debe1bc57e4993b53f37877ecc
SivaneshDevanL <71081217+SivaneshDevanL@users.noreply.github.com>
2021-05-13 00:36:21 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -2,12 +2,28 @@ import React, {Component, useState} from "react";
 import "./../styles/App.css";
 
 function App() {
-  return (
-    <div id="main">
-      // Do not alter the main div
+  const [state, setState] = useState();
+    function Para2() {
+        setState("can")
+    }
+    return (<div id="main">
+        <button style={{ marginLeft: '45%', marginTop: '20%' }} id="click" onClick={Para2}>click me</button>
+        {state === "can" && (
+            <Para />
+        )
+        }
     </div>
-  );
+    )
 }
+
+
+function Para() {
+    return (
+        <p style={{ marginLeft: '30%', marginTop: '1%' }} id="para">Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy
+        </p>
+    )
+}
+
 
 
 export default App;
